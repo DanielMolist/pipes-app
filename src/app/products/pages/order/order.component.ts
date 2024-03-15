48b9eecfbd3547497ce7d9020430ddd4bcda2fb3
Daniel Molist <Daniel.Molist@gmail.com>
2024-03-15 18:45:30 +0100

    Fin sección 12

---
+++ src/app/products/pages/order/order.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styles: ``,
+})
+export class OrderComponent {
+  public isUpperCase: boolean = false;
+  public sortBy: keyof Hero | null = null;
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue,
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black,
+    },
+    {
+      name: 'Dar Devil',
+      canFly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.green,
+    },
+    {
+      name: 'Linterna verde',
+      canFly: true,
+      color: Color.green,
+    },
+  ];
+
+  toggleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  setSortBy(value: keyof Hero): void{
+    this.sortBy = value;
+  }
+}
--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { OrderComponent } from './pages/order/order.component';
 
 const routes: Routes = [
   {
@@ -19,9 +20,13 @@ const routes: Routes = [
     component: UncommonPageComponent,
   },
   {
+    path: 'custom',
+    component: OrderComponent,
+  },
+  {
     path: '**',
-    redirectTo: ''
-  }
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
